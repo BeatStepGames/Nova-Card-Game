845961a1f4f8ad2619849f23d8cf8f1aadfc3dcb
MarcoLoPinto <30478903+MarcoLoPinto@users.noreply.github.com>
2017-10-17 18:41:36 +0200

    Updated field

--- Server/server resources/Field.js
+++ Server/server resources/Field.js
@@ -1,23 +1,19 @@
 class Field extends GameObject {
 	//x is CENTRAL
 	constructor(x, y, cardWidth, cardHeight, rows, columns){
-		super(x, y, columns*(cardWidth + (cardWidth*(1/15)*2) ), rows*(cardHeight + (cardWidth*(1/15)*2) ) );
+		super(x, y, columns*(cardWidth), rows*(cardHeight) );
 		
 		this.fieldCards = new Array();
 		this.collisionMasks = new Array();
 		
 		this.rows = rows;
 		this.columns = columns;
-		this.paddingRatio = 1/15;
 		
 		this.originalCardWidth = cardWidth;
 		this.originalCardHeight = cardHeight;
 		this.cardWidth = cardWidth;
 		this.cardHeight = cardHeight;
-		//this.padding = cardWidth*this.paddingRatio;
-		
-		//this.areaFieldX = this.cardWidth+(this.padding*2); //width of an AreaField (where you place the card)
-		//this.areaFieldY = this.cardHeight+(this.padding*2); //height of an AreaField
+
 		this.borderX = this.x-(this.width/2); //border x of the field, not the center x
 		//this.borderY = this.y-(this.height/2); //border y of the field, not the center y
 		this.borderY = this.y;
@@ -73,15 +69,13 @@ class Field extends GameObject {
 		//for(var j=0;j<=1;j++){
 			for(var i=0;i<this.columns;i++){
 				if(field.collisionMasks[j+""+i].contains(x,y) && field.fieldCards[j+""+i] == undefined){
-					//card.x = this.collisionMasks[j+""+i].x + this.padding;
-					//card.y = this.collisionMasks[j+""+i].y + this.padding;
 					
 					card.setCenterX(this.collisionMasks[j+""+i].x+this.collisionMasks[j+""+i].width/2);
 					card.setCenterY(this.collisionMasks[j+""+i].y+this.collisionMasks[j+""+i].height/2);
 					
 					field.fieldCards[j+""+i] = card;
 					
-					//field.fieldCards[j+""+i].zoom(field.fieldCards[j+""+i].width*2,field.fieldCards[j+""+i].height*2,100); //DEBUG ANIMATION TEST HERE!!! <----
+					field.fieldCards[j+""+i].zoom(field.fieldCards[j+""+i].width*2,field.fieldCards[j+""+i].height*2,100); //DEBUG ANIMATION TEST HERE!!! <----
 					
 					hand_cards.handStack.remove(card._stackID);
 					server.sendMessage("debug card_palced");
@@ -99,14 +93,11 @@ class Field extends GameObject {
 		
 		this.cardHeight = this.originalCardHeight*sizeFactor;
 		this.cardWidth = this.originalCardWidth*sizeFactor;
-		//this.padding = this.cardWidth*this.paddingRatio;
-		//this.areaFieldX = this.cardWidth;
-		//this.areaFieldY = this.cardHeight;
 		this.borderX = this.x-(this.width/2);
 		//this.borderY = this.y-(this.height/2);
 		this.borderY = this.y;
 		
-		this.fieldArea.update(this.borderX ,this.borderY ,this.cardWidth*this.columns,this.cardHeight*this.rows);
+		this.fieldArea.update(this.borderX,this.borderY ,this.cardWidth*this.columns,this.cardHeight*this.rows);
 		
 		for(var j=0;j<this.rows;j++){
 			for(var i=0;i<this.columns;i++){
@@ -117,13 +108,9 @@ class Field extends GameObject {
 		for(var j=0;j<this.rows;j++){
 			for(var i=0;i<this.columns;i++){
 				if(field.fieldCards[j+""+i] != undefined){
-					//this.fieldCards[j+""+i].x = this.collisionMasks[j+""+i].x + this.padding;
-					//this.fieldCards[j+""+i].y = this.collisionMasks[j+""+i].y + this.padding;
 					this.fieldCards[j+""+i].onResize(sizeFactor);
 					this.fieldCards[j+""+i].setCenterX(this.collisionMasks[j+""+i].x + this.cardWidth/2);
 					this.fieldCards[j+""+i].setCenterY(this.collisionMasks[j+""+i].y + this.cardHeight/2);
-					
-					
 					 
 				}
 			}
