afc51dce6dbd134051b0c8336d23151d982781fb
MarcoLoPinto <30478903+MarcoLoPinto@users.noreply.github.com>
2017-10-26 12:24:06 +0200

    Added matchmaking selection

---
+++ Server/server resources/matchmaking.js
@@ -0,0 +1,27 @@
+function setMatchmakingSection(section){
+	
+	let totalContainer = document.getElementById("totalContainer");
+	let allContainers = totalContainer.querySelectorAll(".container");
+	allContainers.forEach(function(element) {
+		element.style.display = "none";
+	}, this);
+	
+	var container;
+	
+	switch(section) {
+		default: // Default is section 0 (return to matchmaking selection page)
+		case 0: 
+			container = document.getElementById("matchmakingPageContainer");
+			break;
+		case 1:
+			container = document.getElementById("friendlySelection");
+			break;
+		case 2:
+			container = document.getElementById("friendlySelection");
+			break;
+		case 3:
+			container = document.getElementById("friendlySelection");
+			break;
+	}
+	container.style.display = "block";
+}
