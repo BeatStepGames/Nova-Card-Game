3fe1140b12aa2834c85973a746a2319521729a41
MarcoLoPinto <30478903+MarcoLoPinto@users.noreply.github.com>
2017-10-30 23:59:20 +0100

    Friendly Page&CSS&JS modified

--- Server/server resources/matchmaking.js
+++ Server/server resources/matchmaking.js
@@ -1,25 +1,45 @@
-function setMatchmakingSection(section){
-	
-	let totalContainer = document.getElementById("totalContainer");
-	let allContainers = totalContainer.querySelectorAll(".container");
-	allContainers.forEach(function(element) {
-		element.style.display = "none";
-	}, this);
-	
-	var container;
-	
-	switch(section) {
-		default: // Default is section 0 (return to matchmaking selection page)
-		case 0: 
-			container = document.getElementById("matchmakingPageContainer");
-			break;
-		case 1:
-			container = document.getElementById("friendlySelection");
-			break;
-		case 2:
-			container = document.getElementById("waitingSelection");
-			break;
-	}
-	container.style.display = "block";
-}
+match = new Matchmaking();
 
+function Matchmaking() {
+	
+	this.setMatchmakingSection = function(section){
+		let totalContainer = document.getElementById("totalContainer");
+		let allContainers = totalContainer.querySelectorAll(".matchmakingContainer");
+		allContainers.forEach(function(element) {
+			element.style.display = "none";
+		}, this);
+		
+		var container;
+		
+		switch(section) {
+			default: // Default is section 0 (return to matchmaking selection page)
+			case 0: 
+				container = document.getElementById("matchSelection");
+				break;
+			case 1:
+				container = document.getElementById("friendlySelection");
+				break;
+			case 2:
+				container = document.getElementById("waitingSelection");
+				break;
+		}
+		container.style.display = "block";
+	}
+	
+	this.loadUserList = function(){
+		server.register("userlist",this.getPlayerList);
+		server.registerOnOpenCallback(server.requestUserList);
+	}
+	
+	this.getPlayerList = function(jsonList,keyword){
+		var arr = JSON.parse(jsonList);
+		var str = "<tr><th>Name</th><th>Rank</th><th>Play</th></tr>";
+		if(keyword == undefined) keyword = "";
+		
+		for(var i = 0; i<arr.length; i++){
+			if(arr[i].username.includes(keyword)) str = str + "<tr>" + "<td>" + arr[i].username + "</td>" + "<td>" + arr[i].rank + "</td>" + "<td>" + "Join" + "</td>" + "\r\n";
+		}
+		document.getElementById("matchmakingLabelClass").innerHTML = str;
+	}
+	
+}
